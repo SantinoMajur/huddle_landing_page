6fda9367e4743b783f73f9fb18c6afc36878c91a
SantinoMajur <dem65109@gmail.com>
2022-05-23 12:48:57 +0300

    Full Responsive Huddle Landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'bg-mobile': "url('../images/bg-mobile.svg')",
+        'bg-desktop': "url('../images/bg-desktop.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
